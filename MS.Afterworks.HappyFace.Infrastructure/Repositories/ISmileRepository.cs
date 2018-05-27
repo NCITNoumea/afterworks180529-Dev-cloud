d357a3bc204c78425d1273b1e480b7b909107e54
Julien CHABLE <julien.chable@ncit.nc>
2018-05-27 22:35:37 +1100

    fix: déplacement projet Infrastructure

---
+++ MS.Afterworks.HappyFace.Infrastructure/Repositories/ISmileRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using MS.Afterworks.HappyFace.Core.Models;
+
+namespace MS.Afterworks.HappyFace.Core.Infrastructure.Repositories
+{
+    public interface ISmileRepository
+    {
+        Task<bool> AddSmileAsync(Smile smile);
+
+        Task<int> CountSmileUp();
+
+        Task<int> CountSmileDown();
+    }
+}
