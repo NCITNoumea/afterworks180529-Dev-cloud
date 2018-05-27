7d156646514aea77dfb579ae9e0020cd41588f9f
Julien CHABLE <julien.chable@ncit.nc>
2018-05-27 17:33:43 +1100

    feat: Ajout de Cognitive Services

---
+++ MS.Afterworks.HappyFace/Services/Cognitive/ApiKeyServiceClientCredentials.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Rest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MS.Afterworks.HappyFace.Services.Cognitive
+{ 
+    public class ApiKeyServiceClientCredentials : ServiceClientCredentials
+    {
+        public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Add("Ocp-Apim-Subscription-Key", Environment.GetEnvironmentVariable("Ocp-Apim-Subscription-Key"));
+            return base.ProcessHttpRequestAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ MS.Afterworks.HappyFace/Services/IServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MS.Afterworks.HappyFace.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MS.Afterworks.HappyFace.Services
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ISmileRepository, SmileRepository>();
+        }
+
+        public static void AddCognitiveServices(this IServiceCollection services)
+        {
+            services.AddScoped<ITextAnalyticsService, TextAnalyticsService>();
+        }
+    }
+}
