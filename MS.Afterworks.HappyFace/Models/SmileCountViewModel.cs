2cd0f5999012b1549212dc68c5b9cbf526a37370
Julien CHABLE <julien.chable@ncit.nc>
2018-05-27 15:58:56 +1100

    feat: Ajout de repository

--- MS.Afterworks.HappyFace/Models/SmileCountViewModel.cs
+++ MS.Afterworks.HappyFace/Models/SmileCountViewModel.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication.Models;
 
 namespace app.Models
 {
     public class SmileCountViewModel
     {
+        public Smile Smile { get; set; }
+
         public int SmileUpCount { get; set; }
 
         public int SmileDownCount { get; set; }
